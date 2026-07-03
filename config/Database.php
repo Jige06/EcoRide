fd32e93336eb5634c77cc613563831ce898c7103
jige06 <jghebert06@gmail.com>
2026-07-03 16:46:49 +0200

    feat: ajout de la classe Database Singleton PDO

--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,44 @@
+<?php
+class Database
+{
+    private static $instance = null;
+    private $pdo;
 
+    private function __construct()
+    {
+        $host = $_ENV['DB_HOST'];
+        $dbname = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USER'];
+        $password = $_ENV['DB_PASSWORD'];
+
+        try {
+            $this->pdo = new PDO(
+                "mysql:host=$host;dbname=$dbname;charset=utf8",
+                $user,
+                $password,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]
+            );
+        } catch (PDOException $e) {
+            die("Erreur de connexion: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if(self::$instance === null) {
+            self::$instance = new Database();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->pdo;
+    }
+
+    private function __clone() {}
+}
